016373412e23805281bb87a9de49d4465d9ae4fd
hoppxi <hoppxii@gmail.com>
2025-07-05 14:01:42 +0300

    add shadow and blur effect

--- modules/home/hyprland/appearance.nix
+++ modules/home/hyprland/appearance.nix
@@ -12,11 +12,20 @@
 
     decoration = {
       rounding = 10;
+      rounding_power = 12;
       blur = {
         enabled = true;
-        size = 8;
-        passes = 3;
+        size = 3;
+        passes = 1;
         vibrancy = 0.1696;
+      };
+      active_opacity = 0.85;
+      inactive_opacity = 0.76;
+      shadow = {
+        enabled = true;
+        range = 4;
+        render_power = 3;
+        color = "rgba(1a1a1aee)";
       };
     };
   };
