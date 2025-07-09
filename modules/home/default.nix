822ca9714e3c27c4e4b25fadf3e9d9919a7584e6
hoppxi <hoppxii@gmail.com>
2025-07-09 14:56:35 +0300

    import services

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -6,6 +6,7 @@
     ./waybar/default.nix
     ./nvim/default.nix
     ./rofi/default.nix
+    ./services/default.nix
     ./shell/zsh.nix
     ./shell/starship.nix
     ./mako.nix
@@ -21,5 +22,14 @@
     username = username;
     homeDirectory = "/home/${username}";
     stateVersion = "25.11";
+  };
+
+  home.file.".config/scripts/updater/flake_updater.sh" = {
+    source = ../../scripts/updater/updater.sh;
+    executable = true;
+  };
+  home.file.".config/scripts/updater/kill.sh" = {
+    source = ../../scripts/updater/kill.sh;
+    executable = true;
   };
 }
