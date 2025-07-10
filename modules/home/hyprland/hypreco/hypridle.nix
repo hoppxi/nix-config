4dffa5a0cd378c2e668d92059aad187d42f0d08a
hoppxi <hoppxii@gmail.com>
2025-07-10 17:26:05 +0300

    use

--- modules/home/hyprland/hypreco/hypridle.nix
+++ modules/home/hyprland/hypreco/hypridle.nix
@@ -1,26 +1,39 @@
 { config, pkgs, ... }:
 
-let
-  hypridleConf = ''
-    general {
-      lock_cmd = pidof hyprlock || hyprlock
-      before_sleep_cmd = pidof hyprlock || hyprlock
-      after_sleep_cmd = hyprctl dispatch dpms on
-      inhibit_sleep = 3
-    }
-
-    listener {
-      timeout = 300
-      on-timeout = pidof hyprlock || hyprlock
-    }
-
-    listener {
-      timeout = 330
-      on-timeout = hyprctl dispatch dpms off
-      on-resume = hyprctl dispatch dpms on
-    }
-  '';
-in
 {
-  xdg.configFile."hypr/hypridle.conf".text = hypridleConf;
+  services.hypridle = {
+    enable = true;
+
+    settings = {
+      general = {
+        lock_cmd = "pidof hyprlock || hyprlock";
+        before_sleep_cmd = "loginctl lock-session";
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+        ignore_dbus_inhibit = false;
+        ignore_systemd_inhibit = false;
+      };
+
+      listener = [
+        {
+          timeout = 150;
+          on-timeout = "brightnessctl -s set 10";
+          on-resume = "brightnessctl -r";
+
+        }
+        {
+          timeout = 300;
+          on-timeout = "loginctl lock-session";
+        }
+        {
+          timeout = 330;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on && brightnessctl -r";
+        }
+        {
+          timeout = 1800;
+          on-timeout = "systemctl suspend";
+        }
+      ];
+    };
+  };
 }
