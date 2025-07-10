bab95d15a6baa057c52397cae889ed419f22ba19
hoppxi <hoppxii@gmail.com>
2025-07-10 17:23:17 +0300

    use hyprcursor

---
+++ modules/home/hyprland/hypreco/hyprcursor.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ... }:
+
+{
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+    name = "Hyprland-Logo";
+    package = pkgs.hyprcursor;
+    size = 32;
+    hyprcursor.enable = true;
+    hyprcursor.size = 32;
+  };
+
+}
