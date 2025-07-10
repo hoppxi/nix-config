b4f163df878d555201b212160f42031a75932168
hoppxi <hoppxii@gmail.com>
2025-07-10 07:46:39 +0300

    packages for nvim

---
+++ modules/home/nvim/packages.nix
@@ -0,0 +1,13 @@
+{ pkgs }:
+
+[
+  pkgs.vimPlugins.neo-tree-nvim
+  pkgs.vimPlugins.bufferline-nvim
+  pkgs.vimPlugins.plenary-nvim
+  pkgs.vimPlugins.tokyonight-nvim
+  pkgs.vimPlugins.telescope-nvim
+  pkgs.vimPlugins.alpha-nvim
+  pkgs.vimPlugins.nvim-lspconfig
+  pkgs.vimPlugins.nvim-web-devicons
+  pkgs.vimPlugins.nui-nvim
+]
