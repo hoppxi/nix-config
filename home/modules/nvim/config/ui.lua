1a7371ff55bc91224b208d6f2b0769068e671e2b
hoppxi <hoppxii@gmail.com>
2025-08-10 13:15:15 +0300

    organize modules

--- home/modules/nvim/config/ui.lua
+++
@@ -1,8 +0,0 @@
-vim.opt.termguicolors = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.signcolumn = "yes"
-vim.opt.cursorline = true
-vim.opt.showmode = false
-vim.opt.laststatus = 2
-vim.opt.cmdheight = 0
