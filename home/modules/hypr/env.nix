be68a92f9085c456939900a40b30a06893f23f4a
hoppxi <hoppxii@gmail.com>
2025-08-10 13:21:42 +0300

    switch to vscode

--- home/modules/hypr/env.nix
+++ home/modules/hypr/env.nix
@@ -5,7 +5,7 @@
     "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
     "WLR_NO_HARDWARE_CURSORS,1"
     "NIXOS_OZONE_WL,1"
-    "EDITOR,nvim"
+    "EDITOR,code"
     "TERMINAL,alacritty"
     "BROWSER,brave"
     "QT_QPA_PLATFORMTHEME,gtk2"
