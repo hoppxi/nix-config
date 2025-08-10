3d257d88cb6c151d819c002402e652a787f52726
hoppxi <hoppxii@gmail.com>
2025-08-10 13:10:34 +0300

    enable xray and dim_inactive

--- home/modules/hypr/appearance.nix
+++ home/modules/hypr/appearance.nix
@@ -13,17 +13,33 @@
       rounding_power = 2;
       blur = {
         enabled = true;
+        xray = true;
+        special = false;
+        new_optimizations = true;
         size = 14;
-        passes = 1;
-        vibrancy = 0.1;
+        passes = 3;
+        brightness = 1;
+        noise = 0.01;
+        contrast = 1;
+        popups = true;
+        popups_ignorealpha = 0.6;
+        input_methods = true;
+        input_methods_ignorealpha = 0.8;
       };
-      active_opacity = 0.8;
-      inactive_opacity = 0.5;
+
+      active_opacity = 0.9;
+      inactive_opacity = 0.7;
+      dim_inactive = true;
+      dim_strength = 0.025;
+      dim_special = 0.07;
+
       shadow = {
         enabled = true;
-        range = 4;
-        render_power = 3;
-        color = "rgba(1a1a1aee)";
+        ignore_window = true;
+        range = 30;
+        offset = "0 2";
+        render_power = 4;
+        color = "rgba(00000010)";
       };
     };
   };
