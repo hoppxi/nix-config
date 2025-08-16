7083aaae1153d2682834661d1b60d5358ad2b42a
hoppxi <hoppxii@gmail.com>
2025-08-17 02:26:42 +0300

    use greetd+tuigreeter

--- hosts/ea/hyprland/session.nix
+++ hosts/ea/hyprland/session.nix
@@ -7,8 +7,17 @@
     dbus.enable = true;
     seatd.enable = true;
 
-    getty.greetingLine = "Welcome to Nixos @ea!";
-    getty.helpLine = "Enter username and password to login.\nRun nixos-help for Nixos manual";
+    # getty.greetingLine = "Welcome to Nixos @ea!";
+    # getty.helpLine = "Enter username and password to login.\nRun nixos-help for Nixos manual";
+    greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd ${pkgs.hyprland}/bin/Hyprland";
+          user = "greeter";
+        };
+      };
+    };
   };
 
   environment.systemPackages = with pkgs; [
