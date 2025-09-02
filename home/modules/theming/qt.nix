bf34c421a9aa19d1c18365acd4fff8c57f5dbd3e
hoppxi <hoppxii@gmail.com>
2025-09-02 09:49:02 +0300

    fix(theming): the new commit in nixpkgs changes breeze-qt5 to breeze-icons and this brings conflict with papirus icons so remove it and use default icons

--- home/modules/theming/qt.nix
+++ home/modules/theming/qt.nix
@@ -7,7 +7,6 @@
     platformTheme.name = "gtk"; # use gtk (through gtk2)
     style = {
       name = "gtk2";
-      package = pkgs.libsForQt5.breeze-qt5; # Breeze style engine for Qt5
     };
   };
 }
