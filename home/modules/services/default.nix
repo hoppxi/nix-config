73c0724359848be07c97539d97e75874895b2d8d
hoppxi <hoppxii@gmail.com>
2025-12-09 19:17:01 +0300

    feat: Remove polling notifications in preparation for event-driven system.

--- home/modules/services/default.nix
+++ home/modules/services/default.nix
@@ -1,31 +1,7 @@
 {
-  pkgs,
-  config,
-  lib,
-  ...
-}:
-
-{
   imports = [
     ./swaync
-    ./status
     ./udiskie
     ./wallpaper
   ];
-
-  home.file = builtins.listToAttrs (
-    map
-      (name: {
-        name = ".config/scripts/services/${name}.sh";
-        value = {
-          source = ../../../scripts/services/${name}.sh;
-          executable = true;
-        };
-      })
-      [
-        "battery"
-        "network"
-        "bluetooth"
-      ]
-  );
 }
